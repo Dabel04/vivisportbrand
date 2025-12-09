37e33ba64cc61321e89adf295b2ca1653c4737cb
dabel <iyiegbudabelchi16@gmail.com>
2025-12-09 12:23:11 +0100

    omo

--- ViviSport-Brand/src/Components/Cart.jsx
+++ ViviSport-Brand/src/Components/Cart.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
 import { useSetAtom } from 'jotai'
 import { productAtom } from '../App'
-
-/* import all the icons in Free Solid, Free Regular, and Brands styles */
-import { fas } from '@fortawesome/free-solid-svg-icons'
 
 library.add(fas)
 
